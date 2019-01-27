bdf282011016e8acddbd71c7138fe702abfc2282
Elana Kopelevich <ekopelevich@gmail.com>
2019-01-26 22:12:41 -0700

    remove private reconciliation implementation; replace with placeholder comment

--- src/Reconciliation/index.ts
+++ src/Reconciliation/index.ts
@@ -10,22 +10,7 @@ export default class Reconciliation {
     this.statements = statements
   }
 
-  // public compare(): void {
-  //   this.checkRegisterEntries(this.register, this.statements)
-  //   this.checkStatementEntries(this.statements, this.register)
-  // }
-
-  // private checkRegisterEntries(register: Register, statement: Statement) {
-  //   this.register.map(registerItem => {
-  //     console.log('hello')
-  //   })
-  // }
-
-  // private checkStatementEntries(statement: Statement, register: Register) {
-  //   this.statements.map(statement => statement.map(statementItem => {
-  //     register.forEach(registerItem => {
-  //       console.log('goodbye')
-  //     })
-  //   })
-  // })
+  public reconcile(): void {
+    // Do the reconciliation
+  }
 }
