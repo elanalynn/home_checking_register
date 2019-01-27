c2a941657532b986fc157a91112b3ed548cd8a6b
Elana Kopelevich <ekopelevich@gmail.com>
2019-01-26 22:21:32 -0700

    add getters and setters

--- src/Statement/index.ts
+++ src/Statement/index.ts
@@ -8,4 +8,28 @@ export default class Statement {
     this.dateIssued = dateIssued
     this.reconciliationId = reconciliationId
   }
+
+  public getbankStatementId(): string {
+    return this.bankStatementId
+  }
+
+  public setbankStatementId(bankStatementId: string): void {
+    this.bankStatementId = bankStatementId
+  }
+
+  public getDateIssued(): string {
+    return this.bankStatementId
+  }
+
+  public setDateIssued(bankStatementId: string): void {
+    this.bankStatementId = bankStatementId
+  }
+
+  public getReconciliationId(): number {
+    return this.reconciliationId
+  }
+
+  public setReconciliationId(reconciliationId: number): void {
+    this.reconciliationId = reconciliationId
+  }
 }
