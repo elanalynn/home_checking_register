03c3eaa18e6e00d61dae5c0a0bc9be59b3212fff
Elana Kopelevich <ekopelevich@gmail.com>
2019-01-26 22:16:57 -0700

    alphabetize members and arguments

--- src/Entry/index.ts
+++ src/Entry/index.ts
@@ -1,16 +1,24 @@
 export default class Entry {
+  private amount: number
   private date: Date
   private description: string
-  private amount: number
   private isReconciled: boolean
 
   constructor(amount: number, description: string, isReconciled: boolean) {
     this.amount = amount
+    this.date = new Date()
     this.description = description
     this.isReconciled = isReconciled
-    this.date = new Date()
   }
 
+  public getAmount(): number {
+    return this.amount
+  }
+
+  public setAmount(amount: number): void {
+    this.amount = amount
+  }
+  
   public getDate(): Date {
     return this.date
   }
@@ -25,14 +33,6 @@ export default class Entry {
 
   public setDescription(description: string): void {
     this.description = description
-  }
-
-  public getAmount(): number {
-    return this.amount
-  }
-
-  public setAmount(amount: number): void {
-    this.amount = amount
   }
 
   public getIsReconciled(): boolean {
